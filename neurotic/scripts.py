9ffcf936626eca8a9c1daba39eecf1cf676b4f7b
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2019-06-27 01:04:10 -0400

    Add basic command line argument parser

--- neurotic/scripts.py
+++ neurotic/scripts.py
@@ -3,13 +3,25 @@
 
 """
 
+import sys
+import argparse
+
+from ephyviewer import mkQApp
+
+from .gui.standalone import DataExplorer
+
 def launch_standalone():
     """
 
     """
 
-    from ephyviewer import mkQApp
-    from .gui.standalone import DataExplorer
+    description = """
+    neurotic lets you curate, visualize, and annotate your behavioral ephys
+    data.
+    """
+    parser = argparse.ArgumentParser(description=description)
+
+    args = parser.parse_args(sys.argv[1:])
 
     app = mkQApp()
     win = DataExplorer()
