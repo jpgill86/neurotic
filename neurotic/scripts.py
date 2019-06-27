b5c2c1f491a053a2808bd40333ece0352bb80d0f
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2019-06-27 02:37:24 -0400

    Add file and dataset positional args to CLI

--- neurotic/scripts.py
+++ neurotic/scripts.py
@@ -21,13 +21,22 @@ def launch_standalone():
     data.
     """
     parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('file', nargs='?', default=None,
+                        help='the path to a metadata YAML file (default: an ' \
+                             'example file)')
+    parser.add_argument('dataset', nargs='?', default=None,
+                        help='the name of a dataset in the metadata file to ' \
+                             'select initially (default: the first entry in ' \
+                             'the metadata file)')
+
     parser.add_argument('-V', '--version', action='version',
                         version='neurotic {}'.format(__version__))
 
     args = parser.parse_args(sys.argv[1:])
 
     app = mkQApp()
-    win = DataExplorer()
+    win = DataExplorer(file=args.file, initial_selection=args.dataset)
     win.show()
     print('Ready')
     app.exec_()
