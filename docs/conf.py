08fcc0779745b5e95012e23d00a14c325030ca8b
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2019-07-25 12:15:57 -0400

    Change docs navigation bg color and add wide logo

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ html_static_path = ['_static']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/neurotic-logo.svg'
+html_logo = '_static/neurotic-logo-banner.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
