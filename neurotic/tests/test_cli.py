b95e879ede87953bc096c0485b7bb777f4ed9b2f
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2021-01-15 11:17:32 -0500

    Use a better default override value for dataset argument

--- neurotic/tests/test_cli.py
+++ neurotic/tests/test_cli.py
@@ -82,7 +82,7 @@ class CLITestCase(unittest.TestCase):
         # substitute special values
         if neurotic.global_config['defaults']['file'] == 'example':
             neurotic.global_config['defaults']['file'] = None
-        if neurotic.global_config['defaults']['dataset'] == 'none':
+        if neurotic.global_config['defaults']['dataset'] == 'first':
             neurotic.global_config['defaults']['dataset'] = None
 
         self.assertEqual(neurotic.global_config,
@@ -256,10 +256,10 @@ class CLITestCase(unittest.TestCase):
                          'dataset was not changed correctly')
 
     def test_example_file_and_first_dataset_overrides(self):
-        """Test that 'example' and 'none' open first dataset in example file"""
+        """Test that 'example' and 'first' open first dataset in example file"""
         neurotic.global_config['defaults']['file'] = 'some other file'
         neurotic.global_config['defaults']['dataset'] = 'some other dataset'
-        argv = ['neurotic', 'example', 'none']
+        argv = ['neurotic', 'example', 'first']
         args = neurotic.parse_args(argv)
         win = neurotic.win_from_args(args)
         self.assertEqual(win.metadata_selector.file, self.example_file,
